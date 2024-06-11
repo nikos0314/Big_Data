dddc401e5047d74cb3fdbe737ca8c4905cc0517b
nikos0314 <149158669+nikos0314@users.noreply.github.com>
2024-06-11 16:02:29 +0300

    Update query_2_df.py

--- code/query_2/query_2_df.py
+++ code/query_2/query_2_df.py
@@ -1,61 +1,47 @@
 import time
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
-from pyspark.sql.types import IntegerType, TimestampType, StringType
+from pyspark.sql.types import IntegerType, StringType
 
 start_time_df = time.time()
 
 spark = (
     SparkSession.builder.appName("Crime Data Analysis Q2 DF")
     .config("spark.sql.debug.maxToStringFields", "1000")
-    .config("spark.executor.memory", "1g")
+    .config("spark.executor.memory", "2g")
     .getOrCreate()
 )
+
+spark.conf.set("spark.sql.shuffle.partitions", "8")
 
 spark.sparkContext.setLogLevel("WARN")
 
 
-crimes_df = spark.read.csv(
-    "hdfs://master:9000/home/user/crime_data", header=True, inferSchema=True
+crime_df = (
+    spark.read.csv(
+        "hdfs://master:9000/home/user/crime_data", header=True, inferSchema=True
+    )
+    .select(F.col("TIME OCC").cast(IntegerType()), F.col("Premis Desc"))
+    .filter((F.col("Premis Desc") == "STREET") & (F.col("TIME OCC").isNotNull()))
+    .sortWithinPartitions("TIME OCC")
 )
 
-
-crime_df = crimes_df.select(F.col("TIME OCC").cast(IntegerType()), F.col("Premis Desc"))
-
-crime_df = crime_df.filter(
-    (F.col("Premis Desc") == "STREET") & (F.col("TIME OCC").isNotNull())
-)
-
-
-def get_time_segment(time_occ):
-    if time_occ is None:
-        return None
-    hour = time_occ
-    if 500 <= hour < 1200:
-        return "Morning"
-    elif 1200 <= hour < 1700:
-        return "Afternoon"
-    elif 1700 <= hour < 2100:
-        return "Evening"
-    else:
-        return "Night"
-
-
-time_segment_udf = F.udf(get_time_segment, StringType())
 
 street_crimes_df = crime_df.withColumn(
-    "time_segment", time_segment_udf(F.col("TIME OCC"))
+    "time_segment",
+    F.when((F.col("TIME OCC") >= 500) & (F.col("TIME OCC") < 1200), "Morning")
+    .when((F.col("TIME OCC") >= 1200) & (F.col("TIME OCC") < 1700), "Afternoon")
+    .when((F.col("TIME OCC") >= 1700) & (F.col("TIME OCC") < 2100), "Evening")
+    .otherwise("Night")
 )
 
 
-result_df = (
-    street_crimes_df.groupBy("time_segment").count().orderBy(F.col("count").desc())
-)
+result_df = street_crimes_df.groupBy("time_segment").count().orderBy(F.col("count").desc())
+
 
 result_df.show()
 
 end_time_df = time.time()
-print(
-    "DataFrame API Execution Time: {:.2f} seconds".format(end_time_df - start_time_df)
-)
+print("DataFrame API Execution Time: {:.2f} seconds".format(end_time_df - start_time_df))
+
 spark.stop()
