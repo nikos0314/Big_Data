b7b2438b6ad1f8cd0a2ed5c43d341ce594df7d8a
nikos0314 <149158669+nikos0314@users.noreply.github.com>
2024-06-11 16:04:04 +0300

    Update query_2_rdd.py

--- code/query_2/query_2_rdd.py
+++ code/query_2/query_2_rdd.py
@@ -1,55 +1,52 @@
 import time
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
-from pyspark.sql.types import IntegerType, TimestampType, StringType
+from pyspark.sql.types import IntegerType
 
 start_time_rdd = time.time()
 
 spark = (
     SparkSession.builder.appName("Crime Data Analysis Q2 RDD")
     .config("spark.sql.debug.maxToStringFields", "1000")
-    .config("spark.executor.memory", "1g")
+    .config("spark.executor.memory", "2g")
+    .config("spark.shuffle.io.netty.eventLoopThreads", "8")
     .getOrCreate()
 )
+
+spark.conf.set("spark.sql.shuffle.partitions", "8")
 
 spark.sparkContext.setLogLevel("WARN")
 
 
-crimes_df = spark.read.csv(
-    "hdfs://master:9000/home/user/crime_data", header=True, inferSchema=True
+crime_df = (
+    spark.read.csv(
+        "hdfs://master:9000/home/user/crime_data", header=True, inferSchema=True
+    )
+    .select(F.col("TIME OCC").cast(IntegerType()), F.col("Premis Desc"))
+    .filter((F.col("Premis Desc") == "STREET") & (F.col("TIME OCC").isNotNull()))
+    .sortWithinPartitions("TIME OCC")
 )
 
-crime_df = crimes_df.select(F.col("TIME OCC").cast(IntegerType()), F.col("Premis Desc"))
 
-street_crimes_rdd = crime_df.filter(
-    (F.col("Premis Desc") == "STREET") & (F.col("TIME OCC").isNotNull())
-).rdd
-
-
-def map_time_segment(row):
-    time_occ = row["TIME OCC"]
-    if time_occ is None:
-        return None
-    hour = time_occ
-    if 500 <= hour < 1200:
-        return ("Morning", 1)
-    elif 1200 <= hour < 1700:
-        return ("Afternoon", 1)
-    elif 1700 <= hour < 2100:
-        return ("Evening", 1)
-    else:
-        return ("Night", 1)
+street_crimes_rdd = crime_df.rdd
 
 
 time_segments_rdd = (
-    street_crimes_rdd.map(map_time_segment)
+    street_crimes_rdd.map(lambda row: (
+        ("Morning" if 500 <= row["TIME OCC"] < 1200 else
+         "Afternoon" if 1200 <= row["TIME OCC"] < 1700 else
+         "Evening" if 1700 <= row["TIME OCC"] < 2100 else
+         "Night"), 1))
     .reduceByKey(lambda a, b: a + b)
     .sortBy(lambda x: -x[1])
 )
 
+
 results = time_segments_rdd.collect()
-for result in results:
-    print(result)
+results_df = spark.createDataFrame(results, ["Time Segment", "Count"])
+
+
+results_df.show()
 
 end_time_rdd = time.time()
 print("RDD API Execution Time: {:.2f} seconds".format(end_time_rdd - start_time_rdd))
