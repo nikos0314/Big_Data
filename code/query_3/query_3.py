9e8a6cd10e721f5138fc7cd796fe5e991afb5a32
nikos0314 <149158669+nikos0314@users.noreply.github.com>
2024-06-11 15:50:40 +0300

    Update query_3.py

--- code/query_3/query_3.py
+++ code/query_3/query_3.py
@@ -1,17 +1,19 @@
-
 import time
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
-from pyspark.sql.types import IntegerType, StringType
-from pyspark.sql.functions import col, to_timestamp, broadcast
+from pyspark.sql.types import IntegerType, StringType, StructType, StructField, DoubleType
+from pyspark.sql.functions import col, to_timestamp, broadcast, when
 
 
 spark = (
     SparkSession.builder.appName("Crime Data Analysis Q3")
     .config("spark.sql.debug.maxToStringFields", "1000")
-    .config("spark.executor.memory", "1g")
+    .config("spark.executor.memory", "2g")
     .getOrCreate()
 )
+
+
+spark.conf.set("spark.sql.shuffle.partitions", "4")
 
 spark.sparkContext.setLogLevel("WARN")
 
@@ -26,55 +28,50 @@ income_df = spark.read.csv(
 
 income_df = income_df.withColumn(
     "Estimated_Median_Income",
-    F.regexp_replace(col("Estimated_Median_Income"), "[$,]", "").cast(IntegerType()),
+    F.regexp_replace(col("Estimated_Median_Income"), "[$,]", "").cast(IntegerType())).cache()
+
+
+
+
+geocoding_schema = StructType([
+    StructField("LAT", DoubleType(), True),
+    StructField("LON", DoubleType(), True),
+    StructField("ZIPcode", StringType(), True)
+])
+
+
+geocoding_df = (
+    spark.read.csv(
+        "hdfs://master:9000/home/user/Big_Data/revgecoding.csv",
+        header=True,
+        schema=geocoding_schema,
+    )
+    .withColumn("ZIP_CODE", F.split(col("ZIPcode"), ";").getItem(0))
+    .drop(F.col("ZIPcode"))
 )
 
-
-geocoding_schema = "LAT DOUBLE, LON DOUBLE, ZIPcode STRING"
-geocoding_df = spark.read.csv(
-    "hdfs://master:9000/home/user/Big_Data/revgecoding.csv",
-    header=True,
-    schema=geocoding_schema,
-)
-
-geocoding_df = geocoding_df.withColumn(
-    "zip_code", F.split(col("ZIPcode"), ";").getItem(0)
-)
-
-geocoding_df = geocoding_df.drop("ZIPcode").withColumnRenamed("zip_code", "ZIP_CODE")
-
-
-crime_df = spark.read.csv(
-    "hdfs://master:9000/home/user/crime_data",
-    header=True,
-    inferSchema=True,
-).withColumn("DATE OCC", F.to_timestamp(F.col("DATE OCC"), "MM/dd/yyyy hh:mm:ss a"))
-
-crime_2015_df = crime_df.filter(F.year(F.col("DATE OCC")) == 2015)
 
 
 crime_2015_df = (
-    crime_2015_df.filter(F.col("Vict Descent").isNotNull())
-    .filter(col("Vict Descent") != "X")
-    .select("AREA NAME", "LAT", "LON", "Vict Descent")
-)
+    spark.read.parquet("hdfs://master:9000/home/user/crime_data_parquet_2015")
+    .filter(F.col("Vict Descent").isNotNull())
+    .filter(F.col("Vict Descent") != "X")
+    .select("LAT", "LON", "Vict Descent")
+    .withColumn("LAT", F.col("LAT").cast("double"))
+    .withColumn("LON", F.col("LON").cast("double"))
+).cache()
 
-
-
-
-crime_2015_df = crime_2015_df.withColumn("LAT", col("LAT").cast("double"))
-crime_2015_df = crime_2015_df.withColumn("LON", col("LON").cast("double"))
 
 crime_with_zip_df = crime_2015_df.join(
     geocoding_df,
     (crime_2015_df.LAT == geocoding_df.LAT) & (crime_2015_df.LON == geocoding_df.LON),
     "left",
-)
+).select(F.col("Vict Descent"), F.col("ZIP_CODE")).cache()
 
 
 
 zip_codes = [
-     row["ZIP_CODE"] for row in crime_with_zip_df.select("ZIP_CODE").distinct().collect()
+     row["ZIP_CODE"] for row in crime_with_zip_df.select("ZIP_CODE").distinct().collect()if row["ZIP_CODE"] is not None
 ]
 
 
@@ -105,9 +102,7 @@ bottom_3_income_crimes_df = crime_with_zip_df.filter(
 
 
 top_3_income_victims = (
-    top_3_income_crimes_df.groupBy("Vict Descent")
-    .count()
-    .orderBy(col("count").desc())
+    top_3_income_crimes_df.groupBy("Vict Descent").count().orderBy(col("count").desc())
 ).withColumnRenamed("count", "total victims")
 
 bottom_3_income_victims = (
@@ -117,40 +112,30 @@ bottom_3_income_victims = (
 ).withColumnRenamed("count", "total victims")
 
 
-def map_descent_code(code):
-    descent_map = {
-        "A": "Asian",
-        "B": "Black",
-        "C": "Chinese",
-        "D": "Cambodian",
-        "F": "Filipino",
-        "G": "Guamanian",
-        "H": "Hispanic/Latin/Mexican",
-        "I": "American Indian/Alaskan Native",
-        "J": "Japanese",
-        "K": "Korean",
-        "L": "Laotian",
-        "O": "Other",
-        "P": "Pacific Islander",
-        "S": "Samoan",
-        "U": "Hawaiian",
-        "V": "Vietnamese",
-        "W": "White",
-        "X": "Unknown",
-        "Z": "Asian Indian",
-    }
-    return descent_map.get(code, "Unknown")
+mapping_expr = when(col("Vict Descent") == "A", "Asian") \
+    .when(col("Vict Descent") == "B", "Black") \
+    .when(col("Vict Descent") == "C", "Chinese") \
+    .when(col("Vict Descent") == "D", "Cambodian") \
+    .when(col("Vict Descent") == "F", "Filipino") \
+    .when(col("Vict Descent") == "G", "Guamanian") \
+    .when(col("Vict Descent") == "H", "Hispanic/Latin/Mexican") \
+    .when(col("Vict Descent") == "I", "American Indian/Alaskan Native") \
+    .when(col("Vict Descent") == "J", "Japanese") \
+    .when(col("Vict Descent") == "K", "Korean") \
+    .when(col("Vict Descent") == "L", "Laotian") \
+    .when(col("Vict Descent") == "O", "Other") \
+    .when(col("Vict Descent") == "P", "Pacific Islander") \
+    .when(col("Vict Descent") == "S", "Samoan") \
+    .when(col("Vict Descent") == "U", "Hawaiian") \
+    .when(col("Vict Descent") == "V", "Vietnamese") \
+    .when(col("Vict Descent") == "W", "White") \
+    .when(col("Vict Descent") == "X", "Unknown") \
+    .when(col("Vict Descent") == "Z", "Asian Indian") \
+    .otherwise("Unknown")
 
-
-map_descent_udf = F.udf(map_descent_code, StringType())
-
-top_3_income_victims = top_3_income_victims.withColumn(
-    "Vict Descent", map_descent_udf(col("Vict Descent"))
-)
-
-bottom_3_income_victims = bottom_3_income_victims.withColumn(
-    "Vict Descent", map_descent_udf(col("Vict Descent"))
-)
+# Apply the mapping to the DataFrames
+top_3_income_victims = top_3_income_victims.withColumn("Vict Descent", mapping_expr)
+bottom_3_income_victims = bottom_3_income_victims.withColumn("Vict Descent", mapping_expr)
 
 
 print("Top 3 Income ZIP Codes Victims by Descent")
@@ -160,13 +145,4 @@ print("Bottom 3 Income ZIP Codes Victims by Descent")
 bottom_3_income_victims.show(10, False)
 
 
-
 spark.stop()
-
-
-
-
-
-
-
-
